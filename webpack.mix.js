6948915f23a136dd9533ec98aa2027f4415b514b
Gus <gustavoocanto@gmail.com>
2021-03-06 11:40:03 +0800

    better structure

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,9 @@
 let mix = require('laravel-mix');
 
 mix
-    .js('src/app.js', 'public')
+    .js('src/app.js', '')
     .vue({ version: 2 })
-    .setPublicPath('public')
+    .setPublicPath('public/js')
     .version()
-    .sourceMaps();
+    .sourceMaps()
+    .extract(['vue']);
