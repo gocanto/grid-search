477876fcf1fefada547d218e5a90246404db280a
Gus <gustavoocanto@gmail.com>
2021-03-07 15:38:41 +0800

    better top-bar

--- resources/js/layout/TopBar.vue
+++ resources/js/layout/TopBar.vue
@@ -1,16 +1,15 @@
 <template>
-  <header class="bg-white h-16 lg:overflow-y-visible lg:static shadow-sm">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="relative flex justify-between xl:grid xl:grid-cols-12 lg:gap-8">
-        <div class="flex md:absolute md:left-0 md:inset-y-0 lg:static xl:col-span-2">
-          <div class="flex-shrink-0 flex items-center">
-            <a href="#">
-              <img class="block h-8 w-auto mt-4" src="images/search.svg?color=indigo&shade=600" alt="Workflow">
-            </a>
+  <header class="bg-white h-16 lg:overflow-y-visible lg:static shadow-sm pt-2">
+      <div class="flex justify-between max-w-7xl mx-auto px-10">
+        <div class="flex items-center">
+          <div>
+              <img src="images/logo.svg" alt="logo" class="h-10 mr-3">
+          </div>
+          <div class="text-2xl font-medium">
+            Products Overview
           </div>
         </div>
       </div>
-    </div>
   </header>
 </template>
 
